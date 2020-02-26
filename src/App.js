db658fd5f1c06e33f90edbdb627c08e803e6e7a2
taiberium <taiberium@mail.ru>
2020-02-26 22:23:38 +0300

    change css style

--- src/App.js
+++ src/App.js
@@ -4,18 +4,11 @@ import 'antd/dist/antd.css';
 import MainPage from "./component/MainPage";
 import {Provider} from 'react-redux';
 import store from './store/store';
-import {Col, Row} from "antd";
 
 function App() {
     return (
         <Provider store={store}>
-            <div className="App">
-                <Row>
-                    <Col offset={4} span={16}>
-                        <MainPage/>
-                    </Col>
-                </Row>
-            </div>
+            <MainPage/>
         </Provider>
     );
 }
--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import DataTable from "./DataTable";
 import SearchInput from "./SearchInput";
+import {Layout} from 'antd';
+import './MainPage.css';
+import MenuComponent from "./MenuComponent";
+
+const {Header, Footer, Content} = Layout;
 
 export default function MainPage(props) {
-    return <div>
-        <SearchInput/>
-        <DataTable/>
-    </div>;
+    return <Layout>
+        {/*
+        <Header style={{backgroundColor: "#FAFAFA", border: "1px solid #D9D9D9"}}>
+
+
+        </Header>*/}
+
+        <MenuComponent style={{backgroundColor: "#FAFAFA", marginLeft: "50px"}}/>
+        <Content style={{padding: "10vw", height: "calc(100vh - 110px)"}}>
+            <SearchInput/>
+            <DataTable/>
+        </Content>
+
+
+        <Footer style={{textAlign: 'center'}}>Made by Taiberium</Footer>
+    </Layout>;
 }
 
 
